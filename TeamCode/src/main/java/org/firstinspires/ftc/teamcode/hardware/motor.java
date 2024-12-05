fc412791e0e813b03e40223c2cc060a1e3c2ae4c
bchan207 <brandonchan207@gmail.com>
2024-12-05 15:43:15 -0500

    motor control class brandon

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/motor.java
@@ -0,0 +1,29 @@
+    package org.firstinspires.ftc.teamcode.hardware;
+
+    import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+    import com.qualcomm.robotcore.hardware.DcMotorEx;
+    import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+    public class motor extends LinearOpMode {
+    DcMotorEx frontLeft, frontRight, backLeft, backRight;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+            frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+            backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+            frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+            backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+            frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+            waitForStart();
+
+            frontLeft.setPower(0.5);
+            frontRight.setPower(0.5);
+            backLeft.setPower(0.5);
+            backRight.setPower(0.5);
+        }
+    }
